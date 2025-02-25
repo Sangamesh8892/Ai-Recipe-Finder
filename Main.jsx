a53b1827e2884a15f5b03b8a6f8c0ddc3a44efc1
sangamesh <demo@example.com>
2025-02-25 14:02:04 +0530

    Added Scroll into View Functionality and Loading div to wait for Api Response

--- Main.jsx
+++ Main.jsx
@@ -7,11 +7,15 @@ export default function Main() {
     const [ingredients, setIngredients] = React.useState(
         []
     )
+    const [loading, setLoading] = React.useState(false)
     const [recipe, setRecipe] = React.useState("")
+    const recipeSection =React.useRef(null)
 
     async function getRecipe() {
+        setLoading(true);
         const recipeMarkdown = await getRecipeFromMistral(ingredients)
         setRecipe(recipeMarkdown)
+        setLoading(false);
 
     }
     function removeIngredient(ingredientToRemove) {
@@ -24,7 +28,10 @@ export default function Main() {
         setIngredients(prevIngredients => [...prevIngredients, newIngredient])
         event.target.reset()
     }
-
+    React.useEffect(() => {
+        if(recipe && recipeSection.current !== null)
+        {  recipeSection.current.scrollIntoView({behavior: "smooth"})} 
+    },[recipe, recipeSection.current])
     return (
         <main>
             <form onSubmit={addIngredient} className="add-ingredient-form">
@@ -39,12 +46,13 @@ export default function Main() {
 
             {ingredients.length > 0 &&
                 <IngredientsList
+                    recipeSection={recipeSection}
                     ingredients={ingredients}
                     getRecipe={getRecipe}
                     removefunc={removeIngredient}
                 />
             }
-
+            {loading && <p>Loading...</p>}
             {recipe && <ClaudeRecipe recipe={recipe} />}
         </main>
     )
